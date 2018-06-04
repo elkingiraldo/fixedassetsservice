b5163b5a0a6ee3c6b71f8336d4aa64698d36de7b
egiraldo <egiraldo@localhost>
2018-06-04 13:31:40 -0500

    base spring boot application

---
+++ fixed-assets-service/src/main/java/co/com/grupoasd/services/fixedassets/FixedAssetsApplication.java
@@ -0,0 +1,12 @@
+package co.com.grupoasd.services.fixedassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedAssetsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FixedAssetsApplication.class, args);
+	}
+
+}
